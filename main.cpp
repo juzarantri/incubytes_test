02cbfc26fff32aa32766d63db802c148addc2f6b
iamjuzarantri <juzarantri786@gmail.com>
2022-07-16 10:19:53 +0530

    2 and 3 done

--- main.cpp
+++ main.cpp
@@ -2,25 +2,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int add(string &s){
-    if(s == "") return 0;
-    vector<int> nums;
-    size_t pos = 0;
-    string delimeter = ",";
-    while((pos = s.find(delimeter)) != string::npos){
-        string currNum = s.substr(0,pos);
-        nums.push_back(stoi(currNum));
-        s.erase(0,pos+delimeter.length());
+void getNumsSeperatedWithNewLine(vector<int> &nums, string s)
+{
+    size_t n = s.length();
+    size_t i;
+    for (i = 0; i < n; i++)
+    {
+        if (s[i] == 92)
+        {
+            string currNum = s.substr(0, i);
+            nums.push_back(stoi(currNum));
+            s.erase(0, i+2);
+            getNumsSeperatedWithNewLine(nums,s);
+            break;
+        }
     }
-    nums.push_back(stoi(s));
-    return accumulate(nums.begin(),nums.end(),0);
+    if(i == n)
+        nums.push_back(stoi(s));
+}
+
+void getNumsSeperatedWithComma(vector<int> &nums, string &s, string &delimeter)
+{
+    size_t pos = 0;
+    while ((pos = s.find(delimeter)) != string::npos)
+    {
+        string currNum = s.substr(0, pos);
+        //// getting numbers seperated by newLine
+        getNumsSeperatedWithNewLine(nums, currNum);
+        s.erase(0, pos + delimeter.length());
+    }
+    
+    getNumsSeperatedWithNewLine(nums,s);
+    
+}
+
+int add(string &s)
+{
+    if (s == "")
+        return 0;
+    vector<int> nums;
+    string delimeter = ",";
+
+    //// getting numbers seperated by comma
+    getNumsSeperatedWithComma(nums, s, delimeter);
+    
+
+    return accumulate(nums.begin(), nums.end(), 0);
 }
 
 int main()
 {
     string s;
-    std::cin >> s;
-    
+    getline(cin, s);
+
     cout << add(s) << endl;
 
     return 0;
